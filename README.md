56d53575391621dd3308cc9d6f84f090213f331f
Mark Lenzer <ich@madmexx.com>
2021-08-28 12:54:49 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # APEX-Shuttle-with-Caption
-This plug-in is used to set custom captions over/under the selection area of the APEX Shuttle Item to give the user better explanation.
+![](Screenshot_1.PNG)
+This plug-in is used to set custom captions over/under the selection area of the APEX Shuttle Item to give the user a better explanation.
+
+## Setup
+
+* Create Dynamic Action on Page Load
+* Selet Action "APEX Shuttle with Caption [Plug-In]"
+* Set your custom captions under "Settings" 
+* Select the shuttle under "Affected Elements"
+
+![](Screenshot_2.PNG)
---
+++ dynamic_action_plugin_com_madmexx_shuttle_caption.sql
@@ -0,0 +1,213 @@
+prompt --application/set_environment
+set define off verify off feedback off
+whenever sqlerror exit sql.sqlcode rollback
+--------------------------------------------------------------------------------
+--
+-- ORACLE Application Express (APEX) export file
+--
+-- You should run the script connected to SQL*Plus as the Oracle user
+-- APEX_200100 or as the owner (parsing schema) of the application.
+--
+-- NOTE: Calls to apex_application_install override the defaults below.
+--
+--------------------------------------------------------------------------------
+begin
+wwv_flow_api.import_begin (
+ p_version_yyyy_mm_dd=>'2016.08.24'
+,p_release=>'5.1.3.00.05'
+,p_default_workspace_id=>254300323815878746
+,p_default_application_id=>666
+,p_default_id_offset=>0
+,p_default_owner=>'ML_DEV'
+);
+end;
+/
+ 
+--   Name:            APEX Shuttle with Caption
+--   Date and Time:   12:43 Saturday August 28, 2021
+
+begin
+  -- replace components
+  wwv_flow_api.g_mode := 'REPLACE';
+end;
+/
+prompt --application/shared_components/plugins/dynamic_action/com_madmexx_shuttle_caption
+begin
+wwv_flow_api.create_plugin(
+ p_id=>wwv_flow_api.id(411100288218599367)
+,p_plugin_type=>'DYNAMIC ACTION'
+,p_name=>'COM.MADMEXX.SHUTTLE_CAPTION'
+,p_display_name=>'APEX Shuttle with Caption'
+,p_category=>'STYLE'
+,p_supported_ui_types=>'DESKTOP:JQM_SMARTPHONE'
+,p_plsql_code=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'FUNCTION F_RENDER (',
+'    P_DYNAMIC_ACTION   IN APEX_PLUGIN.T_DYNAMIC_ACTION,',
+'    P_PLUGIN           IN APEX_PLUGIN.T_PLUGIN',
+') RETURN APEX_PLUGIN.T_DYNAMIC_ACTION_RENDER_RESULT AS',
+'    L_RESULT         APEX_PLUGIN.T_DYNAMIC_ACTION_RENDER_RESULT;',
+'    L_UPPER         varchar2(256);',
+'    L_LOWER         varchar2(256);',
+'BEGIN',
+'    -- Upper Caption',
+'    IF P_DYNAMIC_ACTION.ATTRIBUTE_01 is not null OR P_DYNAMIC_ACTION.ATTRIBUTE_02 is not null then',
+'        -- Left Column',
+'        L_UPPER := ''$(''''<tr><td class="'' || P_DYNAMIC_ACTION.ATTRIBUTE_05 || ''" '' ||  P_DYNAMIC_ACTION.ATTRIBUTE_06  || ''">'' || APEX_ESCAPE.HTML_WHITELIST( P_DYNAMIC_ACTION.ATTRIBUTE_01 ) || ''</td>'';',
+'        -- Middle Column',
+'        L_UPPER := L_UPPER || ''<td></td>'';',
+'        -- Right Column',
+'        L_UPPER := L_UPPER || ''<td class="'' || P_DYNAMIC_ACTION.ATTRIBUTE_05 || ''" '' ||  P_DYNAMIC_ACTION.ATTRIBUTE_06  || ''">'' || APEX_ESCAPE.HTML_WHITELIST( P_DYNAMIC_ACTION.ATTRIBUTE_02 ) || ''</td></tr'''').insertBefore(item +'''' .shuttle tbody'''');'';',
+'    end if;',
+'',
+'    -- Lower Caption',
+'    IF P_DYNAMIC_ACTION.ATTRIBUTE_03 is not null OR P_DYNAMIC_ACTION.ATTRIBUTE_04 is not null then',
+'        -- Left Column',
+'        L_LOWER := ''$(''''<tr><td class="'' || P_DYNAMIC_ACTION.ATTRIBUTE_05 || ''" '' ||  P_DYNAMIC_ACTION.ATTRIBUTE_06  || ''">'' || APEX_ESCAPE.HTML_WHITELIST( P_DYNAMIC_ACTION.ATTRIBUTE_03 ) || ''</td>'';',
+'        -- Middle Column',
+'        L_LOWER := L_LOWER || ''<td></td>'';',
+'        -- Right Column',
+'        L_LOWER := L_LOWER || ''<td class="'' || P_DYNAMIC_ACTION.ATTRIBUTE_05 || ''" '' ||  P_DYNAMIC_ACTION.ATTRIBUTE_06  || ''">'' || APEX_ESCAPE.HTML_WHITELIST( P_DYNAMIC_ACTION.ATTRIBUTE_04 ) || ''</td></tr'''').insertAfter(item +'''' .shuttle tbody'''');'';',
+'    end if;',
+'',
+'        L_RESULT.JAVASCRIPT_FUNCTION   := ''',
+'        function () { ',
+'            var item = "#" + $(this)[0].action.affectedElements;',
+'            '' || L_UPPER || L_LOWER || ''',
+'            console.info(''''Set Caption for Shuttle: '''' + item); ',
+'        }''; ',
+'',
+'    RETURN L_RESULT;',
+'END;'))
+,p_api_version=>2
+,p_render_function=>'F_RENDER'
+,p_standard_attributes=>'ITEM'
+,p_substitute_attributes=>true
+,p_subscribe_plugin_settings=>true
+,p_help_text=>'This plug-in is used to set custom captions over/under the selection area of the APEX Shuttle Item to give the user better explanation.'
+,p_version_identifier=>'1.0'
+,p_about_url=>'https://github.com/madmexx2002/APEX-Shuttle-with-Caption'
+,p_files_version=>11
+);
+wwv_flow_api.create_plugin_attribute(
+ p_id=>wwv_flow_api.id(411300418184637864)
+,p_plugin_id=>wwv_flow_api.id(411100288218599367)
+,p_attribute_scope=>'COMPONENT'
+,p_attribute_sequence=>1
+,p_display_sequence=>10
+,p_prompt=>'Upper Left Caption'
+,p_attribute_type=>'TEXT'
+,p_is_required=>false
+,p_default_value=>'Upper Left Caption'
+,p_max_length=>255
+,p_is_translatable=>true
+,p_help_text=>'Caption left over shuttle.'
+);
+wwv_flow_api.create_plugin_attribute(
+ p_id=>wwv_flow_api.id(411300771591639473)
+,p_plugin_id=>wwv_flow_api.id(411100288218599367)
+,p_attribute_scope=>'COMPONENT'
+,p_attribute_sequence=>2
+,p_display_sequence=>20
+,p_prompt=>'Upper Right Caption'
+,p_attribute_type=>'TEXT'
+,p_is_required=>false
+,p_default_value=>'Upper Right Caption'
+,p_max_length=>255
+,p_is_translatable=>true
+,p_help_text=>'Caption right over shuttle.'
+);
+wwv_flow_api.create_plugin_attribute(
+ p_id=>wwv_flow_api.id(411500295203656481)
+,p_plugin_id=>wwv_flow_api.id(411100288218599367)
+,p_attribute_scope=>'COMPONENT'
+,p_attribute_sequence=>3
+,p_display_sequence=>30
+,p_prompt=>'Lower Left Caption'
+,p_attribute_type=>'TEXT'
+,p_is_required=>false
+,p_default_value=>'Lower Left Caption'
+,p_max_length=>255
+,p_is_translatable=>true
+,p_help_text=>'Caption left under shuttle.'
+);
+wwv_flow_api.create_plugin_attribute(
+ p_id=>wwv_flow_api.id(411500934208658156)
+,p_plugin_id=>wwv_flow_api.id(411100288218599367)
+,p_attribute_scope=>'COMPONENT'
+,p_attribute_sequence=>4
+,p_display_sequence=>40
+,p_prompt=>'Lower Right Caption'
+,p_attribute_type=>'TEXT'
+,p_is_required=>false
+,p_default_value=>'t-Form-inlineHelp'
+,p_max_length=>255
+,p_is_translatable=>true
+,p_help_text=>'Caption right under shuttle.'
+);
+wwv_flow_api.create_plugin_attribute(
+ p_id=>wwv_flow_api.id(412100234859939625)
+,p_plugin_id=>wwv_flow_api.id(411100288218599367)
+,p_attribute_scope=>'COMPONENT'
+,p_attribute_sequence=>5
+,p_display_sequence=>50
+,p_prompt=>'CSS Classes'
+,p_attribute_type=>'TEXT'
+,p_is_required=>false
+,p_max_length=>255
+,p_is_translatable=>false
+,p_help_text=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'Enter classes to add to this component. You may add multiple classes by separating them with spaces.<br>',
+'Note: These CSS classes will be applied to the HTML td element tag.<br>'))
+);
+wwv_flow_api.create_plugin_attribute(
+ p_id=>wwv_flow_api.id(412100935681941046)
+,p_plugin_id=>wwv_flow_api.id(411100288218599367)
+,p_attribute_scope=>'COMPONENT'
+,p_attribute_sequence=>6
+,p_display_sequence=>60
+,p_prompt=>'Custom Attributes'
+,p_attribute_type=>'TEXT'
+,p_is_required=>false
+,p_max_length=>255
+,p_is_translatable=>false
+,p_help_text=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'Enter additional attributes to be included in the form element HTML tag.<br>',
+'For the class HTML attribute it is better to use the dedicated CSS Classes attribute instead.'))
+);
+end;
+/
+begin
+wwv_flow_api.g_varchar2_table := wwv_flow_api.empty_varchar2_table;
+wwv_flow_api.g_varchar2_table(1) := '4D4954204C6963656E73650A0A436F70797269676874202863292032303231204D61726B204C656E7A65720A0A5065726D697373696F6E20697320686572656279206772616E7465642C2066726565206F66206368617267652C20746F20616E79207065';
+wwv_flow_api.g_varchar2_table(2) := '72736F6E206F627461696E696E67206120636F70790A6F66207468697320736F66747761726520616E64206173736F63696174656420646F63756D656E746174696F6E2066696C657320287468652022536F66747761726522292C20746F206465616C0A';
+wwv_flow_api.g_varchar2_table(3) := '696E2074686520536F66747761726520776974686F7574207265737472696374696F6E2C20696E636C7564696E6720776974686F7574206C696D69746174696F6E20746865207269676874730A746F207573652C20636F70792C206D6F646966792C206D';
+wwv_flow_api.g_varchar2_table(4) := '657267652C207075626C6973682C20646973747269627574652C207375626C6963656E73652C20616E642F6F722073656C6C0A636F70696573206F662074686520536F6674776172652C20616E6420746F207065726D697420706572736F6E7320746F20';
+wwv_flow_api.g_varchar2_table(5) := '77686F6D2074686520536F6674776172652069730A6675726E697368656420746F20646F20736F2C207375626A65637420746F2074686520666F6C6C6F77696E6720636F6E646974696F6E733A0A0A5468652061626F766520636F70797269676874206E';
+wwv_flow_api.g_varchar2_table(6) := '6F7469636520616E642074686973207065726D697373696F6E206E6F74696365207368616C6C20626520696E636C7564656420696E20616C6C0A636F70696573206F72207375627374616E7469616C20706F7274696F6E73206F662074686520536F6674';
+wwv_flow_api.g_varchar2_table(7) := '776172652E0A0A54484520534F4654574152452049532050524F564944454420224153204953222C20574954484F55542057415252414E5459204F4620414E59204B494E442C2045585052455353204F520A494D504C4945442C20494E434C5544494E47';
+wwv_flow_api.g_varchar2_table(8) := '20425554204E4F54204C494D4954454420544F205448452057415252414E54494553204F46204D45524348414E544142494C4954592C0A4649544E45535320464F52204120504152544943554C415220505552504F534520414E44204E4F4E494E465249';
+wwv_flow_api.g_varchar2_table(9) := '4E47454D454E542E20494E204E4F204556454E54205348414C4C205448450A415554484F5253204F5220434F5059524947485420484F4C44455253204245204C4941424C4520464F5220414E5920434C41494D2C2044414D41474553204F52204F544845';
+wwv_flow_api.g_varchar2_table(10) := '520A4C494142494C4954592C205748455448455220494E20414E20414354494F4E204F4620434F4E54524143542C20544F5254204F52204F54484552574953452C2041524953494E472046524F4D2C0A4F5554204F46204F5220494E20434F4E4E454354';
+wwv_flow_api.g_varchar2_table(11) := '494F4E20574954482054484520534F465457415245204F522054484520555345204F52204F54484552204445414C494E475320494E205448450A534F4654574152452E0A';
+null;
+end;
+/
+begin
+wwv_flow_api.create_plugin_file(
+ p_id=>wwv_flow_api.id(412900585368165617)
+,p_plugin_id=>wwv_flow_api.id(411100288218599367)
+,p_file_name=>'LICENSE.txt'
+,p_mime_type=>'text/plain'
+,p_file_charset=>'utf-8'
+,p_file_content=>wwv_flow_api.varchar2_to_blob(wwv_flow_api.g_varchar2_table)
+);
+end;
+/
+prompt --application/end_environment
+begin
+wwv_flow_api.import_end(p_auto_install_sup_obj => nvl(wwv_flow_application_install.get_auto_install_sup_obj, false));
+commit;
+end;
+/
+set verify on feedback on define on
+prompt  ...done
